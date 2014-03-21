06c8ce989ae3dd834ba8d7d7596413c9ebb82477
Brian Wu <brianwu02@gmail.com>
2014-03-20 22:47:58 -0400

    adding column traversal boilerplate, or whatever you call it

--- blur.cpp
+++ blur.cpp
@@ -14,12 +14,9 @@ int main(int argc, char** argv) {
     int xsz, ysz;
     imageP I1, I2, tmp_img;
 
-    I1 = IP_readImage(argv[1]);
-
     xsz = atoi(argv[2]);
     ysz = atoi(argv[3]);
 
-    
     // bitwise operator to check if xsz & ysz are odd.
     if  (((xsz & 1 ) == 0) or ((ysz & 1) == 0)) {
         // xsz or ysz are even.
@@ -29,6 +26,8 @@ int main(int argc, char** argv) {
         exit(1);
     }
 
+
+    I1 = IP_readImage(argv[1]);
     I2 = NEWIMAGE;
     tmp_img = NEWIMAGE;
 
@@ -45,10 +44,13 @@ int main(int argc, char** argv) {
 
 void blur(imageP I1, imageP tmp_img, int xsz, int ysz, imageP I2) {
 
-    int i, total;
-    unsigned char *in, *out;
+    int i, total, sum;
+    unsigned char *in, *out, *tmp;
 
     total = I1->width * I1->height;
+
+    tmp_img->height = I1->height;
+    tmp_img->width = I1->width;
     
     I2->width = I1->width;
     I2->height = I2->height;
@@ -57,14 +59,44 @@ void blur(imageP I1, imageP tmp_img, int xsz, int ysz, imageP I2) {
 
     out = I2->image;
     in = I1->image;
+    tmp = tmp_img->image;
+
 
     if (I2->image == NULL) {
         cerr << "not enough mem\n";
         exit(1);
     }
+    int w = I1->width;
+    int h = I1->height;
     
+    int s = (xsz - 1) / 2;
+    int t = (ysz - 1) / 2;
+
+    short buf[w+s];
+
+    cout << s << " " << t << endl;
+
+
+    for (int i=0; i<w; i++) {
+        for (int x=2; x<w-2; x++) {
+            out[x] = (in[x-2] + in[x-1] + in[x] + in[x+1] + in[x+2]) / 5;
+            sum += (in[x+3] - in[x-2]);
+        }
+    }
+
+    for (int j=0; i<h; j++) {
+        for (int y=2; y<h-2; y++) {
+            // do stuff here
+        }
+    }
+
+
+
+
+
     // first, scan the image horizontally and store output in temp buffer.
-    
+
+
 
 
 
