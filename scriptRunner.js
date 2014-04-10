ec8023b6160ad5798702fc33c157e228ebfccfcd
Brian Wu <brianwu02@gmail.com>
2014-04-10 11:56:15 -0400

    changed script so that input images are taken image folder and outputs are written to output folder.

--- scriptRunner.js
+++ scriptRunner.js
@@ -12,6 +12,26 @@ var async = require('async');
 console.time('Script Execution Time');
 var tasks = [];
 var cmds = [
+  './unordered_dither images/mad256.pgm 8 .5 output/out1.pgm',
+  './ordered_dither images/mad256.pgm 3 .5 output/out2.pgm',
+  './ordered_dither images/mad256.pgm 4 .5 output/out3.pgm',
+  './ordered_dither images/mad256.pgm 8 .5 output/out4.pgm',
+  './halftone images/eye50.pgm 4 .4 output/out5.pgm',
+  './halftone images/eye50.pgm 8 .4 output/out6.pgm',
+  './error_diffusion images/mad256.pgm 1 1 .5 output/out8.pgm',
+  './blur images/mandrill256.pgm 5 5 output/out9.pgm',
+  './blur images/mandrill256.pgm 5 1 output/out10.pgm',
+  './blur images/mandrill256.pgm 1 5 output/out11.pgm',
+  './sharpen images/mad256.pgm 5 3 output/out12.pgm',
+  './median images/maddot.pgm 3 0 output/out13.pgm',
+  './convolve images/mad256.pgm file1.AF output/out14.pgm',
+  './convolve images/mad256.pgm file2.AF output/out15.pgm',
+  './convolve images/mad256.pgm file3.AF output/out16.pgm',
+  './convolve images/mad256.pgm file4.AF output/out17.pgm'
+];
+
+/*
+var cmds = [
   './unordered_dither mad256.pgm 8 .5 out1.pgm',
   './ordered_dither mad256.pgm 3 .5 out2.pgm',
   './ordered_dither mad256.pgm 4 .5 out3.pgm',
@@ -29,7 +49,7 @@ var cmds = [
   './convolve mad256.pgm file3.AF out16.pgm',
   './convolve mad256.pgm file4.AF out17.pgm'
 ];
-
+*/
 // Generate exec function from each command
 cmds.forEach(function(data) {
     tasks.push(function(callback) {
