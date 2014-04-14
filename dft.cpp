2cbdf3e5d96d13e7390606444e2c132435967e0e
Brian Wu <brianwu02@gmail.com>
2014-04-13 20:23:34 -0400

    add strcut

--- dft.cpp
+++ dft.cpp
@@ -10,3 +10,11 @@ using namespace std;
 int main(int argc, char** argv) {
     int i;
 }
+
+typedef struct {
+    int len; // length of complex number list
+    float *real; // pointer to real number list
+    float *imag; // pointer to imaginary number list
+} complexS, *complexP;
+
+
